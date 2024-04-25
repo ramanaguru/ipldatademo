24cdfc1df014b13443ff6215a76fc3f2e0689400
ramanaguru <ramanaguruloyola@gmail.com>
2024-04-25 13:11:47 +0530

    Initial Commit

---
+++ src/main/java/com/example/ipldatademo/IpldatademoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ipldatademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpldatademoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpldatademoApplication.class, args);
+    }
+
+}
