2ccdaf392b97843b2907f5158e81a71196652a02
ramanaguru <ramanaguruloyola@gmail.com>
2024-04-25 16:30:51 +0530

    Step - 3 //Spring Batch

---
+++ src/main/java/com/example/ipldatademo/batch/BatchConfig.java
@@ -0,0 +1,100 @@
+package com.example.ipldatademo.batch;
+
+import com.example.ipldatademo.models.Match;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class BatchConfig {
+    //Step -3 : Put Together a Batch Job
+
+
+    private final String[] FIELD_NAMES = new String[]{
+            "id",
+            "city",
+            "date",
+            "player_of_match",
+            "venue",
+            "neutral_venue",
+            "team1",
+            "team2",
+            "toss_winner",
+            "toss_decision",
+            "winner",
+            "result",
+            "result_margin",
+            "eliminator",
+            "method",
+            "umpire1",
+            "umpire2",
+    };
+
+    @Bean
+    public FlatFileItemReader<MatchInput> reader() {
+        return new FlatFileItemReaderBuilder<MatchInput>()
+                .name("matchItemReader")
+                .resource(new ClassPathResource("ipl_match_data.csv"))
+                .delimited()
+                .names(FIELD_NAMES)
+                .fieldSetMapper(new BeanWrapperFieldSetMapper<>(){{
+                    setTargetType(MatchInput.class);
+                }})
+                .build();
+    }
+
+    @Bean
+    public MatchDataProcessor processor() {
+        return new MatchDataProcessor();
+    }
+
+    @Bean
+    public JdbcBatchItemWriter<Match> writer(DataSource dataSource) {
+        JdbcBatchItemWriter<Match> writer = new JdbcBatchItemWriter<>();
+        writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>());
+        writer.setSql("INSERT INTO matches(id, city, date, player_of_match, venue, team1, team2, toss_winner, "
+                + "toss_decision, winner, result, result_margin, umpire1, umpire2) "
+                + "VALUES (:id, :city, :date, :playerOfMatch, :venue, :team1, :team2, :tossWinner, :tossDecision, "
+                + ":winner, :result, :resultMargin, :umpire1, :umpire2)");
+        writer.setDataSource(dataSource);
+        return writer;
+    }
+
+    @Bean
+    public Job importUserJob(JobRepository jobRepository,
+                             JobCompletionNotificationListener listener, Step step1) {
+        return new JobBuilder("importUserJob" , jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .listener(listener)
+                .flow(step1)
+                .end()
+                .build();
+    }
+
+    @Bean
+    public Step step1(JobRepository jobRepository,
+                      PlatformTransactionManager transactionManager, JdbcBatchItemWriter<Match> writer) {
+        return new StepBuilder("step1", jobRepository)
+                .<MatchInput, Match> chunk(10, transactionManager) //chunk() method specifies the number of items to be processed in each transaction.
+                .reader(reader())
+                .processor(processor())
+                .writer(writer)
+                .transactionManager(transactionManager)
+                .build();
+    }
+
+}
