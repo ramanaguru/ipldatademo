d29ad45bae7230d9d850fe5f30e761d27b923f34
ramanaguru <ramanaguruloyola@gmail.com>
2024-04-26 12:18:18 +0530

    Updated APP.js && APP.css

---
+++ src/frontend/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import {BrowserRouter as  Router, Routes ,Route, } from 'react-router-dom'
+import {TeamPage} from "./pages/TeamPage";
+import {MatchPage} from "./pages/MatchPage";
+import {HomePage} from "./pages/HomePage";
+
+function App() {
+    return (
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route path = "/teams/:teamName" element={<TeamPage />} />
+                    <Route path= "/teams/:teamName/matches/:year" element={<MatchPage/>} />
+                    <Route path = "/teams" element={<HomePage/>} />
+                </Routes>
+            </Router>
+        </div>
+    );
+}
+
+export default App;
